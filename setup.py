73ae02e9aba1bf50f6a7d58451f64b88a4231722
Benjamin Turner <benturn@gmail.com>
2021-03-06 15:24:16 -0800

    herpderp

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ try:
     from setuptools import setup
 except ImportError:
     from ez_setup import use_setuptools
+
     use_setuptools()
     from setuptools import setup
 
@@ -11,40 +12,34 @@ except ImportError:
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name='django-portfolio',
-    version='0.9.2',
-    description='Portfolio management for web workers.',
-    url='https://github.com/benspaulding/django-portfolio/',
-    author='Ben Spaulding',
-    author_email='ben@benspaulding.us',
-    license='BSD',
-    download_url='http://github.com/benspaulding/django-portfolio/tarball/v0.9.2',
-    long_description=read('README.rst'),
-    packages=['portfolio', 'portfolio.migrations', 'portfolio.tests'],
+    name="django-portfolio",
+    version="0.9.2",
+    description="Portfolio management for web workers.",
+    url="https://github.com/benspaulding/django-portfolio/",
+    author="Ben Spaulding",
+    author_email="ben@benspaulding.us",
+    license="BSD",
+    download_url="http://github.com/benspaulding/django-portfolio/tarball/v0.9.2",
+    long_description=read("README.rst"),
+    packages=["portfolio", "portfolio.migrations", "portfolio.tests"],
     package_data={
-        'portfolio': [
-            'locale/*/LC_MESSAGES/*',
-            'templates/portfolio/*',
-        ],
+        "portfolio": ["locale/*/LC_MESSAGES/*", "templates/portfolio/*",],
     },
-    install_requires=[
-        'Django>=1.4.2,<1.10',
-        'Pillow',
-        'genericglue',
-    ],
+    install_requires=["Django>=1.4.2", "Pillow", "genericglue",],
     dependency_links=[
-        'https://github.com/blturner/django-genericglue/tarball/master#egg=genericglue-0.0.2',
+        "https://github.com/blturner/django-genericglue/tarball/master#egg=genericglue-0.0.2",
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Internet :: WWW/HTTP :: Site Management",
     ],
 )
