73e33a0eac0a05e67519a840f0c03ee4a862e3d5
Benjamin Turner <benturn@gmail.com>
2025-05-23 21:35:22 -0700

    Update admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,12 +1,19 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from django.utils.translation import ugettext_lazy as _
-from django.utils.translation import ugettext_noop, ungettext
+from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_noop, ngettext
 from genericglue.admin import WithGenericObjectForm
 
 from .constants import STATUS_CHOICES, DRAFTED, PUBLISHED, REMOVED
-from .models import Client, Testimonial, Medium, Discipline, Project, ProjectPiece
+from .models import (
+    Client,
+    Testimonial,
+    Medium,
+    Discipline,
+    Project,
+    ProjectPiece,
+)
 
 
 def update_status(modeladmin, request, queryset, status):
@@ -23,22 +30,27 @@ def update_status(modeladmin, request, queryset, status):
         obj.save()
         # Now log what happened.
         # Use ugettext_noop() 'cause this is going straight into the db.
-        log_message = ugettext_noop('Changed status to \'%s\'.' %
-            obj.get_status_display())
+        log_message = gettext_noop(
+            "Changed status to '%s'." % obj.get_status_display()
+        )
         modeladmin.log_change(request, obj, log_message)
 
     # Send a message to the user telling them what has happened.
     message_dict = {
-        'count': queryset.count(),
-        'object': modeladmin.model._meta.verbose_name,
-        'verb': dict(STATUS_CHOICES)[status],
+        "count": queryset.count(),
+        "object": modeladmin.model._meta.verbose_name,
+        "verb": dict(STATUS_CHOICES)[status],
     }
-    if not message_dict['count'] == 1:
-        message_dict['object'] = modeladmin.model._meta.verbose_name_plural
-    user_message = ungettext(
-        '%(count)s %(object)s was successfully %(verb)s.',
-        '%(count)s  %(object)s were successfully %(verb)s.',
-        message_dict['count']) % message_dict
+    if not message_dict["count"] == 1:
+        message_dict["object"] = modeladmin.model._meta.verbose_name_plural
+    user_message = (
+        ngettext(
+            "%(count)s %(object)s was successfully %(verb)s.",
+            "%(count)s  %(object)s were successfully %(verb)s.",
+            message_dict["count"],
+        )
+        % message_dict
+    )
     modeladmin.message_user(request, user_message)
 
     # Return None to display the change list page again and allow the user
@@ -50,19 +62,25 @@ def update_status(modeladmin, request, queryset, status):
 def draft(modeladmin, request, queryset):
     """Admin action for setting status of selected items to 'drafted'."""
     return update_status(modeladmin, request, queryset, DRAFTED)
-draft.short_description = _('Draft selected %(verbose_name_plural)s')
+
+
+draft.short_description = _("Draft selected %(verbose_name_plural)s")
 
 
 def publish(modeladmin, request, queryset):
     """Admin action for setting status of selected items to 'published'."""
     return update_status(modeladmin, request, queryset, PUBLISHED)
-publish.short_description = _('Publish selected %(verbose_name_plural)s')
+
+
+publish.short_description = _("Publish selected %(verbose_name_plural)s")
 
 
 def remove(modeladmin, request, queryset):
     """Admin action for setting status of selected items to 'removed'."""
     return update_status(modeladmin, request, queryset, REMOVED)
-remove.short_description = _('Remove selected %(verbose_name_plural)s')
+
+
+remove.short_description = _("Remove selected %(verbose_name_plural)s")
 
 
 class TestimonialInline(admin.StackedInline):
@@ -71,52 +89,82 @@ class TestimonialInline(admin.StackedInline):
 
 
 class PortfolioBaseAdmin(admin.ModelAdmin):
-    list_display = ('name', )
+    list_display = ("name",)
     list_per_page = 50
-    prepopulated_fields = {'slug': ('name', )}
-    search_fields = ('name', )
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ("name",)
 
 
 class ClientAdmin(PortfolioBaseAdmin):
-    fieldsets = (
-        (None, {'fields': ('name', 'slug', 'url')}),
-    )
-    inlines = (TestimonialInline, )
-    list_display = ('name', 'url')
-    search_fields = ('name', 'url')
+    fieldsets = ((None, {"fields": ("name", "slug", "url")}),)
+    inlines = (TestimonialInline,)
+    list_display = ("name", "url")
+    search_fields = ("name", "url")
 
 
 class ProjectPieceInlineForm(WithGenericObjectForm):
     class Meta:
         model = ProjectPiece
-        fields = ('project',)
+        fields = ("project",)
 
 
 class ProjectPieceInline(admin.StackedInline):
     model = ProjectPiece
     form = ProjectPieceInlineForm
-    fields = ('object', )
+    fields = ("object",)
     ct_field = "object_type"
     ct_fk_field = "object_id"
 
 
 class ProjectAdmin(PortfolioBaseAdmin):
     actions = (draft, publish, remove)
-    date_hierarchy = 'completion_date'
+    date_hierarchy = "completion_date"
     fieldsets = (
-        (None, {'fields': ('name', 'slug', 'client', 'project_url',
-            ('completion_date', 'is_ongoing'), 'status')}),
-        (_('Content'), {'fields': ('summary', 'description', 'overview_image',
-            'detail_image')}),
-        (_('Categorization'), {'fields': ('media', 'disciplines')}),
+        (
+            None,
+            {
+                "fields": (
+                    "name",
+                    "slug",
+                    "client",
+                    "project_url",
+                    ("completion_date", "is_ongoing"),
+                    "status",
+                )
+            },
+        ),
+        (
+            _("Content"),
+            {
+                "fields": (
+                    "summary",
+                    "description",
+                    "overview_image",
+                    "detail_image",
+                )
+            },
+        ),
+        (_("Categorization"), {"fields": ("media", "disciplines")}),
     )
-    filter_horizontal = ('disciplines', )
-    list_display = ('name', 'client', 'completion_date', 'status',
-        'in_development')
-    list_filter = ('status', 'completion_date', 'is_ongoing', 'media',
-        'disciplines')
-    inlines = [ProjectPieceInline, ]
-    search_fields = ('name', 'summary', 'description')
+    filter_horizontal = ("disciplines",)
+    list_display = (
+        "name",
+        "client",
+        "completion_date",
+        "status",
+        "in_development",
+    )
+    list_filter = (
+        "status",
+        "completion_date",
+        "is_ongoing",
+        "media",
+        "disciplines",
+    )
+    inlines = [
+        ProjectPieceInline,
+    ]
+    search_fields = ("name", "summary", "description")
 
 
 admin.site.register(Medium, PortfolioBaseAdmin)
