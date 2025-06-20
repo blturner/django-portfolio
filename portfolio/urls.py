d70e772cc796c978f9db9d2f602fa4558b0b649d
Benjamin Turner <benturn@gmail.com>
2025-06-19 21:55:26 -0700

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,14 +1,17 @@
 from __future__ import unicode_literals
 
-from django.conf.urls import url
+from django.urls import re_path
 from django.views.generic.list import ListView
 
 from .models import Project
 
 
 urlpatterns = [
-    url(r'^$',
-        ListView.as_view(context_object_name='project',
-                         queryset=Project.objects.published()),
-        name='portfolio-project-list'),
+    re_path(
+        r"^$",
+        ListView.as_view(
+            context_object_name="project", queryset=Project.objects.published()
+        ),
+        name="portfolio-project-list",
+    ),
 ]
